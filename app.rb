4ec2f561f3f6208a5a38d68ad7d9e6c67c09c4fa
jason ho <43012595+JasonHo404@users.noreply.github.com>
2023-09-22 17:12:45 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,9 +1,22 @@
 require "sinatra"
 require "sinatra/reloader"
+require 'http'
+require 'json'
+
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+erb(:home)
+end
+
+get('/output') do
+
+  
+  ENV.fetch("GMAPS_KEY") 
+  gmap_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + params.fetch("location") +"&key=" + ENV.fetch("GMAPS_KEY") 
+
+
+  raw_response = HTTP.get(gmap_url)
+  @parsed_response = JSON.parse(raw_response)
+  ENV.fetch("SKY_KEY") 
+  erb(:location)
 end
