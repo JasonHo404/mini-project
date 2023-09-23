b7f50bf859cd626961d3efe31c89aa18e1095502
jason ho <43012595+JasonHo404@users.noreply.github.com>
2023-09-23 15:45:37 +0000

    EW

--- app.rb
+++ app.rb
@@ -33,16 +33,7 @@ get('/output') do
 
 
   ENV.fetch("KEY") 
+
+
   erb(:location)
-  uri = URI("https://api.astronomyapi.com/api/v2/studio/star-chart")
-
-  x = HTTP.auth('Basic YjRhYjc1YzUtNjVjYy00NWQ5LWFjNzAtYWNiYWFlMjk2MmNmOmMyNzFmMjgyZjNlZTM5ZWRhNzZmMTQ1ZDU2NDc1OTI2NzZhZThmNDExNjNjZjBkNTNmNmNhMGU3MjkxOThhYjc4YmQxNzI0Mzg5MTIyMjMzZDk1OWYyODkwMmYyMGU5OTVjMzljYThkOWJhYTgwNzk0NjhmNjA3MzRmMDk3YjkxYWE0ZGRkMWU5NGQwYjg0MWY2MTFiYmU0ODZhNzBmY2UzMWZiZGM0NDRkYWJmMzUyYmZhNmE3M2JmZTRkZjZkMzk0ZWVmYzE2MjdmNjg3NGFhNjA1Y2NiOWIyZDMwMGQ1')
-
-
-
-  body = "{\"style\":\"navy\",\"observer\":{\"latitude\":33.775867,\"longitude\":-84.39733,\"date\":\"2023-09-22\"},\"view\":{\"type\":\"area\",\"parameters\":{\"position\":{\"equatorial\":{\"rightAscension\":0,\"declination\":0}},\"zoom\":\"0\"}}}"
-
-  response = HTTP.post(uri, :params => {:Authorization => x, :body => body })
-
-  
 end
